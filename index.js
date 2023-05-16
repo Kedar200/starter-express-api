a6364cec8a4dcf024e4578b7a354a95ddc3fc302
Kedar200 <83753201+Kedar200@users.noreply.github.com>
2023-05-16 09:58:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 });
-app.all("/city_list", async function (req, res) {
-  res.send("Data Here");
-});
+
 app.listen(process.env.PORT || 3000)

5c6f48d7af174c971416ec42c50e624e2442e27e
Kedar200 <83753201+Kedar200@users.noreply.github.com>
2023-05-16 09:51:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
+
+const adddata = require("./AddData");
+const fileUpload = require("express-fileupload");
+const createfolder = require("./makefolder");
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
-})
+});
+app.get("/city_list", async function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  const data = await adddata.distinct("city");
+  res.status(200).json(data);
+});
 app.listen(process.env.PORT || 3000)
